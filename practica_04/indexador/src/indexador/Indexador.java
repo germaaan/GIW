cee075b8e83b4d037962da82975f6e4eeea5e87a
German Martinez <germaaan@gmail.com>
2016-05-02 13:25:35 +0200

    Finaliza índice

--- practica_04/indexador/src/indexador/Indexador.java
+++ practica_04/indexador/src/indexador/Indexador.java
@@ -2,11 +2,16 @@ package indexador;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.lucene.analysis.es.SpanishAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -22,33 +27,47 @@ public class Indexador {
 
     Parser parser = new Parser();
 
-    public Indexador(String documentos, String palabrasVacias, String indice) throws IOException {
-        SpanishAnalyzer analyzer = new SpanishAnalyzer(Version.LUCENE_43,
-                parser.getPalabrasVacias(palabrasVacias));
+    public Indexador(String documentos, String palabrasVacias, String indice) {
+        try {
+            SpanishAnalyzer analyzer = new SpanishAnalyzer(Version.LUCENE_43,
+                    parser.getPalabrasVacias(palabrasVacias));
 
-        System.out.println("Número de palabras vacías reconocidas: " + analyzer.getStopwordSet().size());
+            System.out.println("Número de palabras vacías reconocidas: " + analyzer.getStopwordSet().size());
 
-        Directory directory = FSDirectory.open(new File(indice));
-        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_43, analyzer);
+            System.out.println("Convirtiendo a XML todos los archivos SGML...");
+            ArrayList<String> archivos = new ArrayList(convertirDocumentos(documentos));
 
-        System.out.println("Convirtiendo a XML todos los archivos SGML...");
-        ArrayList<String> archivos = new ArrayList(convertirDocumentos(documentos));
+            System.out.println("Recuperando toda la información de los archivos...");
+            ArrayList<Noticia> noticias = new ArrayList(procesarDocumentos(documentos, archivos));
 
-        System.out.println("Recuperando toda la información de los archivos...");
-        ArrayList<Noticia> noticias = new ArrayList(procesarDocumentos(documentos, archivos));
+            System.out.println("Número de noticias recuperadas: " + noticias.size());
 
-        System.out.println("Número de noticias recuperadas: " + noticias.size());
+            Directory directory = FSDirectory.open(new File(indice));
 
-        System.out.println("Borrando archivos temporales...");
-        eliminarTemporales(documentos, archivos);
+            IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_43, analyzer);
+            IndexWriter iwriter = new IndexWriter(directory, config);
 
-        /*
-        FSDirectory directory = FSDirectory.open(new File(rutaIndice));
-        IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_43, analizador);
+            Iterator<Noticia> iterador = noticias.iterator();
+            while (iterador.hasNext()) {
+                Noticia noticia = iterador.next();
 
-        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
-        this.iwriter = new IndexWriter(directory, config);
-         */
+                Document doc = new Document();
+
+                doc.add(new Field("titulo", noticia.getTitulo(), Field.Store.YES, Field.Index.ANALYZED));
+                //Añadir tokenstream filters
+                doc.add(new TextField("texto", new StringReader(noticia.getTexto())));
+
+                iwriter.addDocument(doc);
+            }
+
+            System.out.println("Número de documentos indexados: " + iwriter.numDocs());
+            System.out.println("Borrando archivos temporales...");
+            eliminarTemporales(documentos, archivos);
+            System.out.println("Indexado finalizado.");
+            iwriter.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Indexador.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     private ArrayList<String> convertirDocumentos(String documentos) {
@@ -90,14 +109,10 @@ public class Indexador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            String documentos = "/home/germaaan/GIW/documentos/";
-            String palabrasVacias = "/home/germaaan/GIW/palabras_vacias_utf8.txt";
-            String indice = "/home/germaaan/GIW/indice";
+        String documentos = "/home/germaaan/GIW/documentos/";
+        String palabrasVacias = "/home/germaaan/GIW/palabras_vacias_utf8.txt";
+        String indice = "/home/germaaan/GIW/indice";
 
-            Indexador indexador = new Indexador(documentos, palabrasVacias, indice);
-        } catch (IOException ex) {
-            Logger.getLogger(Indexador.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        Indexador indexador = new Indexador(documentos, palabrasVacias, indice);
     }
 }
