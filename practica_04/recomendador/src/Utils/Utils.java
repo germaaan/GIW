e37217e17b274caf45bfbd8ec9d8b9574a2835cb
German Martinez <germaaan@gmail.com>
2016-06-08 14:51:49 +0200

    Lectura de peliculas mediante clase

--- practica_04/recomendador/src/Utils/Utils.java
+++ practica_04/recomendador/src/Utils/Utils.java
@@ -1,9 +1,40 @@
 package Utils;
 
+import data.Pelicula;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import recomendador.Recomendador;
+
 /**
  *
  * @author Germán Martínez Maldonado
  */
 public class Utils {
-    
+
+    public ArrayList<Pelicula> cargarPeliculas() throws UnsupportedEncodingException, IOException {
+        ArrayList<Pelicula> peliculas = new ArrayList();
+
+        InputStream is = Recomendador.class.getResourceAsStream("/resources/u.item");
+        BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+        String entrada = br.readLine();
+        while (entrada != null) {
+            String[] item = entrada.split("\\|");
+
+            int id = Integer.parseInt(item[0]);
+            String nombre = item[1];
+
+            peliculas.add(new Pelicula(id, nombre));
+
+            entrada = br.readLine();
+        }
+
+        br.close();
+        
+        return peliculas;
+    }
 }
--- practica_04/recomendador/src/recomendador/Recomendador.java
+++ practica_04/recomendador/src/recomendador/Recomendador.java
@@ -1,54 +1,35 @@
 package recomendador;
 
+import Utils.Utils;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import data.Pelicula;
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Random;
-import java.util.Scanner;
 
 /**
  *
  * @author Germán Martínez Maldonado
  */
 public class Recomendador {
-    private ArrayList<Pelicula> peliculas;
 
-    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException, IOException {
-        HashMap<Integer, String> peliculas = new HashMap<>();
-        Multimap<Integer, int[]> valoraciones = HashMultimap.create();
-        HashMap<Integer, Integer> valoracionesUsuario = new HashMap<>();
+    private Utils utils = new Utils();
 
-        InputStream is = Recomendador.class.getResourceAsStream("/resources/u.item");
-        BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-
-        String entrada = br.readLine();
-        while (entrada != null) {
-            String[] item = entrada.split("\\|");
-
-            int id = Integer.parseInt(item[0]);
-            String nombre = item[1];
-
-            peliculas.put(id, nombre);
-
-            entrada = br.readLine();
-        }
-
-        br.close();
+    public static void main(String[] args) throws IOException {
+        Recomendador recomendador = new Recomendador();
+        //HashMap<Integer, String> peliculas = new HashMap<>();
+        //Multimap<Integer, int[]> valoraciones = HashMultimap.create();
+        //HashMap<Integer, Integer> valoracionesUsuario = new HashMap<>();
 
         /*
         for (Map.Entry<Integer, String> entry : peliculas.entrySet()) {
         System.out.println(entry.getKey() + " : " + entry.getValue());
         }
          */
+ /*
         is = Recomendador.class.getResourceAsStream("/resources/u.data");
         br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
 
@@ -105,6 +86,10 @@ public class Recomendador {
 
         //calcular metricas
         //seleccionar vecinos
-        //calcular predicciones
+        //calcular predicciones*/
+    }
+
+    public Recomendador() throws IOException {
+        ArrayList<Pelicula> peliculas = new ArrayList<>(utils.cargarPeliculas());
     }
 }
