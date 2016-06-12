3b6c894ccd7263f93ed4f8cc5e73267bc50d23bf
German Martinez <germaaan@gmail.com>
2016-06-13 01:11:14 +0200

    Documenta clase principal

--- practica_04/recomendador/src/recomendador/Recomendador.java
+++ practica_04/recomendador/src/recomendador/Recomendador.java
@@ -6,27 +6,48 @@ import data.Usuario;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
+ * Clase principal del sistema de recomendación.
  *
  * @author Germán Martínez Maldonado
  */
 public class Recomendador {
 
+    // Clase de métodos de apoyo.
     private final Utils utils = new Utils();
 
-    public static void main(String[] args) throws IOException {
+    /**
+     * Método principal del sistema de recomendación.
+     */
+    public static void main(String[] args) {
         Recomendador recomendador = new Recomendador();
     }
 
-    public Recomendador() throws IOException {
-        ArrayList<Pelicula> peliculas = new ArrayList<>(utils.cargarPeliculas());
-        ArrayList<Usuario> usuarios = new ArrayList<>(utils.cargarUsuarios());
-        HashMap<Integer, Integer> calificacionesUsuario = new HashMap<>(utils.introducirValoraciones(peliculas));
-
-        utils.calculaSimilitudes(usuarios, calificacionesUsuario);
-        ArrayList<Usuario> vecinos = new ArrayList<>(utils.seleccionarVecinos(usuarios));
-
-        utils.seleccionarRecomendaciones(vecinos, calificacionesUsuario, peliculas);
+    /**
+     * Ejecuta el sistema de recomendación para mostrar recomendaciones después
+     * de introducir las valoraciones de 15 películas aleatorias.
+     */
+    public Recomendador(){
+        try {
+            // Carga todas las películas
+            ArrayList<Pelicula> peliculas = new ArrayList<>(utils.cargarPeliculas());
+            // Carga todos los usuarios con sus valoraciones
+            ArrayList<Usuario> usuarios = new ArrayList<>(utils.cargarUsuarios());
+            HashMap<Integer, Integer> calificacionesUsuario = new HashMap<>(utils.introducirValoraciones(peliculas));
+            
+            // Calcula la similitud de todos los usuarios con el usuario actual
+            utils.calculaSimilitudes(usuarios, calificacionesUsuario);
+            // Selecciona los 10 usuarios más similares al usuario actual
+            ArrayList<Usuario> vecinos = new ArrayList<>(utils.seleccionarVecinos(usuarios));
+            
+            // Selecciona las 5 películas más recomendables en función de las
+            // valoraciones dadas por los usuarios similares
+            utils.seleccionarRecomendaciones(vecinos, calificacionesUsuario, peliculas);
+        } catch (IOException ex) {
+            Logger.getLogger(Recomendador.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
